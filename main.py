7aaf7348e5eb004e7bf6f3bff0c8e38dfb7cf133
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-09-23 12:37:02 -0400

    createing a graph

--- main.py
+++ main.py
@@ -2,10 +2,32 @@ import requests
 
 # created this project by following the "How to use API doc at https://pixe.la/"
 
+#create my account
 parametersPost = {"token": "krutarthpixelaapikey4567891230",
                   "username": "imgp004",
                   "agreeTermsOfService": "yes",
                   "notMinor": "yes",}
 
 responePost = requests.post(url="https://pixe.la/v1/users", json=parametersPost)
-print(responePost.json())
+# print(responePost.json())
+
+#create my graph
+USERNAME = "imgp004"
+TOKEN = "krutarthpixelaapikey4567891230"
+graph_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
+parametersGraph={
+    "id": "graph1",
+    "name": "My Learning Streak",
+    "unit": "commit",
+    "type": "int",
+    "color": "sora",
+    "startOnMonday": True,
+}
+headersGraph={
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=parametersGraph, headers=headersGraph)
+# print(response.json())
+
+# Our graph is now available at https://pixe.la/v1/users/imgp004/graphs/graph1.html
